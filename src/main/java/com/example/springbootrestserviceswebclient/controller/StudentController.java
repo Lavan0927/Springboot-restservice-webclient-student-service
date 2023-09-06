d524e5c2cd0114ecb0754cb86404b83895d1abeb
Lavanya Ravichandran <rlavan0927@gmail.com>
2023-09-06 23:27:19 +0530

    swagger Configured

--- src/main/java/com/example/springbootrestserviceswebclient/controller/StudentController.java
+++ src/main/java/com/example/springbootrestserviceswebclient/controller/StudentController.java
@@ -1,20 +1,33 @@
 package com.example.springbootrestserviceswebclient.controller;
 
+import com.example.springbootrestserviceswebclient.entity.Student;
 import com.example.springbootrestserviceswebclient.response.StudentResponse;
 import com.example.springbootrestserviceswebclient.service.StudentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/student")
 public class StudentController {
     @Autowired
     private StudentService studentService;
 
-    @GetMapping("/student/{id}")
+    @GetMapping("/{id}")
+    @Operation(summary = "Get department info by providing studentId",security = @SecurityRequirement(name = "Authorization"), responses =
+            {@ApiResponse(content = @Content(schema = @Schema(implementation = Student.class)))})
+    @ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Successfully retrieved student by Id"),
+            @ApiResponse(responseCode = "400", description = "Student not found") })
     private ResponseEntity<StudentResponse> getStudentDetails(@PathVariable("id") int id){
         StudentResponse student = studentService.getStudentById(id);
         return ResponseEntity.status(HttpStatus.OK).body(student);
